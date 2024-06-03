811e880b8afd5ff901c34d103ace08498881c3a9
Robert Cronin <robert.cronin@uqconnect.edu.au>
2024-06-03 10:19:00 +1000

    Update bootstrap

--- argocd/bootstrap.sh
+++ argocd/bootstrap.sh
@@ -5,7 +5,7 @@ set -o errexit
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-kubectl apply -f "$(dirname $0)/../apps.yaml"
+kubectl apply -f "$(dirname $0)/../bootstrap.yaml"
 
 # # Log in using the CLI
 # password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)
