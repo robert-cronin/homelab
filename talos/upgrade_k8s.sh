5b43fd6c3dff246778ae4afa6e10a2a4aff3d22a
Robert Cronin <robert.cronin@uqconnect.edu.au>
2024-06-08 11:36:23 +1000

    Add fluxcd cluster dir

--- talos/upgrade_k8s.sh
+++ talos/upgrade_k8s.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
 # Copyright 2024 Robert Cronin
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+NODE_IP=192.168.86.166 # Control Plane Node IP
+K8S_VERSION=v1.25.16 # Kubernetes Version
 
-# https://github.com/getsops/sops
-mkdir -p $GOPATH/src/github.com/getsops/sops/
-git clone https://github.com/getsops/sops.git $GOPATH/src/github.com/getsops/sops/
-cd $GOPATH/src/github.com/getsops/sops/ && make install && cd -
-
-# brew utils
-brew install fluxcd/tap/flux
+talosctl --nodes $NODE_IP upgrade-k8s --to $K8S_VERSION
--- talos/upgrade_talos.sh
+++ talos/upgrade_talos.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 # Copyright 2024 Robert Cronin
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+IMAGE="ghcr.io/siderolabs/installer:v1.7.4"
 
-# https://github.com/getsops/sops
-mkdir -p $GOPATH/src/github.com/getsops/sops/
-git clone https://github.com/getsops/sops.git $GOPATH/src/github.com/getsops/sops/
-cd $GOPATH/src/github.com/getsops/sops/ && make install && cd -
+# Get IP from argument
+NODE_IP=$1
 
-# brew utils
-brew install fluxcd/tap/flux
+if [[ -z $NODE_IP ]]; then
+    echo "Usage: $0 NODE_IP"
+    exit 1
+fi
+
+talosctl upgrade --nodes $NODE_IP \
+    --image $IMAGE
